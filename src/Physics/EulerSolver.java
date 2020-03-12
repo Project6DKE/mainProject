62a53f5ef863845fdcd150eed211a04c3c03e112
HusamAQ <56165481+HusamAQ@users.noreply.github.com>
2020-03-12 16:58:29 +0100

    fd

---
+++ src/Physics/EulerSolver.java
@@ -0,0 +1,13 @@
+package Physics;
+
+public class EulerSolver implements PhysicsEngine {
+	private double h;
+	public void set_step_size(double h) {this.h=h;}
+	
+	public Vector2d solve(Vector2d vector) {
+		double x=vector.get_x();
+		double y=vector.get_y();
+		
+		return null;
+	}
+	}
---
+++ src/Physics/FunctionH.java
@@ -0,0 +1,17 @@
+package Physics;
+
+public class FunctionH implements Function2d{
+
+	@Override
+	public double evaluate(Vector2d p) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Vector2d gradient(Vector2d p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
