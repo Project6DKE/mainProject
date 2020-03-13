4189067e2e714242000f6bcc300df18b10f4dd7e
MilkyWayVL <56165128+MilkyWayVL@users.noreply.github.com>
2020-03-13 14:33:57 +0100

    CourseRead2 added

---
+++ src/com/Group6/CourseReaderTwo.java
@@ -0,0 +1,74 @@
+package com.Group6;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CourseReaderTwo {
+
+    private static int shotNr;
+    private static double velocity;
+    private static double direction; //coefficient of friction
+
+    public double getShotNr(){
+
+        return shotNr;
+    }
+    public double getVelocity(){
+
+        return velocity;
+    }
+    public double getDirection(){
+
+        return direction;
+    }
+    public static void main(String args[]) {
+        String file = "Speed_velocity.txt"; // change the txt name here
+        Scanner in = new Scanner(System.in);
+        String[] word = new String[1000];
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String st = " ";
+            int i = 0;
+            while ((st = br.readLine()) != null) {
+                word = st.split(" ");
+                switch (i) {
+                    case 0:
+                        for (int j = 0; j < word.length; j++) {
+                            if (word[j].equals("Shot")) {
+                                shotNr = Integer.parseInt(word[j+1]);
+                            }
+                        }
+                        break;
+                    case 1:
+                        for (int j = 0; j < word.length; j++) {
+                            if (word[j].equals("Velocity")) {
+                                velocity = Double.parseDouble(word[j + 2]);
+                            }
+                        }
+                        break;
+                    case 2:
+                        for (int l = 0; l<word.length; l++) {
+                            if (word[l].equals("Direction")) {
+                                direction = Double.parseDouble(word[l + 2]);
+                            }
+                        }
+                        break;
+                }
+                i++;
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        System.out.println(shotNr);
+        System.out.println(velocity);
+        System.out.println(direction);
+
+    }
+}
