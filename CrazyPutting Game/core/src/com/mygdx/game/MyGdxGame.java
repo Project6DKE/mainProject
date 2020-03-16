aa293663e2e427248f5e23ce5a4cab277b9cada5
ThibaultVandeSom <thibaultenmeer@gmail.com>
2020-03-16 21:03:18 +0100

    Besides meshes we can use this to create 2D maps

--- CrazyPutting Game/core/src/com/mygdx/game/MyGdxGame.java
+++ CrazyPutting Game/core/src/com/mygdx/game/MyGdxGame.java
@@ -6,28 +6,39 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
 public class MyGdxGame extends ApplicationAdapter {
+	// As an alternative we can grass or water textures to generate 2D maps
+
+	private static final Object GL10 = 1;
 	SpriteBatch batch;
-	Texture img;
+	Texture grass;
+	Texture water;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		grass = new Texture("grass.jpg");
+		water = new Texture("water.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(grass, 0, 1);
+		batch.draw(water, 0, 100);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		grass.dispose();
+		water.dispose();
 	}
 }
+
+
+
