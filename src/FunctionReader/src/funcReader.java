817c767310734464793eed2b9e296555c780013c
Zigludo123 <nicolasarturopz@gmail.com>
2020-03-17 13:03:57 +0100

    Creation of the two main Readers

---
+++ src/FunctionReader/src/funcReader.java
@@ -0,0 +1,15 @@
+public class funcReader {
+
+    String function;
+
+    public funcReader(String aFunction){
+        this.function = aFunction;
+    }
+
+    public void evaluate(){
+
+
+
+    }
+
+}
