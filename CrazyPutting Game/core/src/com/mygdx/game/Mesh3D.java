058e9ad14efe404dd5f54139bb79cec1a6dbf55c
KristianvK <vankuijk.kristian@gmail.com>
2020-03-18 10:29:02 +0100

    UpdateMesh in 2D

--- CrazyPutting Game/core/src/com/mygdx/game/Mesh3D.java
+++ CrazyPutting Game/core/src/com/mygdx/game/Mesh3D.java
@@ -15,12 +15,13 @@ public class Mesh3D extends ApplicationAdapter {
     Texture texture;
     Sprite sprite;
     Sprite[][] spritearr = new Sprite[10][10];
+    Sprite[][] spritearr2 = new Sprite[10][10];
     Mesh mesh;
     ShaderProgram shaderProgram;
 
 
     OrthographicCamera cam;
-    final Matrix4 matrix = new Matrix4();	
+    //final Matrix4 matrix = new Matrix4();	
 
     @Override
     public void create () {
@@ -30,14 +31,14 @@ public class Mesh3D extends ApplicationAdapter {
         texture = new Texture("grass.jpg");
         sprite = new Sprite(texture);
         sprite.setSize(100, 100);
-        sprite.setSize(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        //sprite.setSize(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
         //cam = new OrthographicCamera(10, 10 * (Gdx.graphics.getHeight() / (float)Gdx.graphics.getWidth()));
         cam = new OrthographicCamera(1920/20, 1080/20);			
 		cam.position.set(5, 5, 10);
 		cam.direction.set(-1, -1, -1);
 		cam.near = 1;
         cam.far = 100;
-        matrix.setToRotation(new Vector3(1, 0, 0), 90);
+        //matrix.setToRotation(new Vector3(1, 0, 0), 00);
 
         float[] verts = new float[30];
         int i = 0;
@@ -95,12 +96,15 @@ public class Mesh3D extends ApplicationAdapter {
         for(int k = 0; k < spritearr.length; k++){
             for(int j = 0; j < spritearr[k].length; j++){
                 spritearr[k][j] = new Sprite(texture);
-				spritearr[k][j].setPosition(k,j);
-				spritearr[k][j].setSize(1, 1);
+                spritearr[k][j].setSize(100, 100);
+				spritearr[k][j].setPosition(k*100,j*100);
+                // spritearr2[k][j] = new Sprite(texture);
+                // spritearr2[k][j].setSize(100,100);
+                // spritearr2[k][j].setPosition(100-k,100-j);
             }
         }
 
-        // Create a mesh out of two triangles rendered clockwise without indices
+         //Create a mesh out of two triangles rendered clockwise without indices
         mesh = new Mesh( true, 6, 0,
                 new VertexAttribute( VertexAttributes.Usage.Position, 3, ShaderProgram.POSITION_ATTRIBUTE ),
                 new VertexAttribute( VertexAttributes.Usage.TextureCoordinates, 2, ShaderProgram.TEXCOORD_ATTRIBUTE+"0" ) );
@@ -117,7 +121,6 @@ public class Mesh3D extends ApplicationAdapter {
     @Override
     public void render () {
         System.out.println("Rendered");
-
         Gdx.gl20.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl20.glClearColor(0.2f, 0.2f, 0.2f, 1);
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -128,22 +131,25 @@ public class Mesh3D extends ApplicationAdapter {
         batch.begin();
         sprite.draw(batch);
         cam.update();		
-				
-		batch.setProjectionMatrix(cam.combined);
-        batch.setTransformMatrix(matrix);
+        
+        //this is important:
+        //batch.setProjectionMatrix(cam.combined);
+        
+        //batch.setTransformMatrix(matrix);
 		for(int z = 0; z < spritearr.length; z++) {
 			for(int x = 0; x < spritearr[z].length; x++) {
-				spritearr[x][z].draw(batch);
+                spritearr[x][z].draw(batch);
+                // spritearr2[x][z].draw(batch);
 			}
 		}
 
-        texture.bind();
-        shaderProgram.begin();
-        shaderProgram.setUniformMatrix("u_projTrans", batch.getProjectionMatrix());
-        shaderProgram.setUniformi("u_texture", 0);
+        //texture.bind();
+        //shaderProgram.begin();
+        //shaderProgram.setUniformMatrix("u_projTrans", batch.getProjectionMatrix());
+        //shaderProgram.setUniformi("u_texture", 0);
 
-        mesh.render(shaderProgram, GL20.GL_TRIANGLES);
+        //mesh.render(shaderProgram, GL20.GL_TRIANGLES);
         batch.end();
-        shaderProgram.end();
+        //shaderProgram.end();
     }
 }
