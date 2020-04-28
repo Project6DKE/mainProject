723c19a4241c8bc1cb44d0227986c400ebfa748d
KristianvK <vankuijk.kristian@gmail.com>
2020-04-28 20:22:45 +0200

    Update GA

--- WorkingGameCrazyPutting/src/MainProject/GA.java
+++ WorkingGameCrazyPutting/src/MainProject/GA.java
@@ -4,13 +4,15 @@ import readingOfFunctions.Function2d;
 import readingOfFunctions.FunctionH;
 
 public class GA {
-	private static int number_of_gen = 10;
+	private static int number_of_gen = 20;
     private static double middle;
-    private static int size_initpopulation = 250;
+    private static int size_initpopulation = 250;	
     private static final int param = 2;
     private int stroke;
     private double mutationrate = 0.01;    
     private double distancefromhole;
+    private double max_fit;
+    private int max_elem;
     private static double[][] initpopulation = new double[size_initpopulation][param];
     private static double[] fitness = new double[initpopulation.length];
     private static double[][] actualpopulation = new double[initpopulation.length][param];
@@ -45,7 +47,9 @@ public class GA {
     
     public void setFitness(){
         middle = 0;
-       System.out.println("The hole x position is at : " + holepos.get_x() + ". The hole y position is at : " + holepos.get_y());
+        max_fit = 0;
+        max_elem = -1;
+        System.out.println("The hole x position is at : " + holepos.get_x() + ". The hole y position is at : " + holepos.get_y());
         for(int i = 0; i < initpopulation.length; i++){
         		ballpos = PS.get_ball_position();
         		backup_ballpos = ballpos;
@@ -54,13 +58,20 @@ public class GA {
         		double getTheY = PS.get_ball_position().get_y();
         		double xshot = holepos.get_x()-getTheX;
         		double yshot = holepos.get_y()-getTheY;
-        		//System.out.println("xshot : " + xshot);
-        		//System.out.println("yshot : " + yshot);
                 distancefromhole = Math.sqrt((Math.pow(xshot, 2) + Math.pow(yshot, 2)));
                 System.out.println("Distance from hole is : " + distancefromhole);
                 fitness[i] = 1/distancefromhole;
                 fitness[i] = fitness[i] * 100;
-                middle = (middle + fitness[i]) / 2;
+                if (i == 0) {
+                	middle = fitness[i];
+                	}
+                else {
+                	middle = (middle + fitness[i])/2;
+                }
+                if ( max_fit < fitness [i]) {
+                	max_fit = fitness[i];
+                	max_elem = i;
+                }
                 System.out.println("Fitness of the element is : " + fitness[i]);
                 PS.set_ball_position(backup_ballpos);
         }
@@ -68,8 +79,14 @@ public class GA {
     
     public void newgen(){
         int n = 0;
+        int chosen1;
         while (n<initpopulation.length || n<actualpopulation.length){
-            int chosen1 = (int) (Math.random() * fitness.length);
+        	if(n==0) {
+        		chosen1 = max_elem;
+        	}
+        	else {
+        		chosen1 = (int) (Math.random() * fitness.length);
+        	}
             int chosen2 = (int) (Math.random() * fitness.length);
             if(fitness[chosen1]>=middle && fitness[chosen2]>=middle){
                 actualpopulation[n][0] = crossover_speed(initpopulation[chosen1][0],initpopulation[chosen2][0]);
@@ -77,6 +94,7 @@ public class GA {
                 n++;
             }
         }
+        
         //convert again to the initpopulation
         for(int i = 0; i<actualpopulation.length ||  i < initpopulation.length; i++){
             for(int j = 0 ; j<actualpopulation[i].length || j < initpopulation[i].length; j++){
@@ -86,12 +104,12 @@ public class GA {
     }
     
     public double crossover_angle(double angle1, double angle2){
-        double newangle = (angle1 + angle2)/2;
+        double newangle = ((4 * angle1) + (6 * angle2))/10;
         return newangle;
     }
 
     public double crossover_speed(double speed1, double speed2) {
-        double newspeed = (speed1 + speed2)/2;
+        double newspeed = ((4 * speed1) + (6 * speed2))/10;
         return newspeed;
     }
     
@@ -150,7 +168,8 @@ public class GA {
        }
        test.setFitness();
        int best = test.bestelement();
-       System.out.println("Best element has fitness : " + fitness[best] + " with a distance to the hole of : ");    
+       double dist = 1/(fitness[best])*100;
+       System.out.println("Best element has fitness : " + fitness[best] + " with a distance to the hole of : " + dist);    
     }
     
 }
