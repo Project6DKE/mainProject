e0d69194f8828f507a7c1d79fd26dfcd494be3b2
HusamAQ <56165481+HusamAQ@users.noreply.github.com>
2020-04-28 20:09:56 +0200

    Added a take shot which returns a List
    The list includes all the positions the ball goes through to reach the final spot (using Euler solver). in addition the addition which nicolas made which makes sure the velocity is within the allowed v, I moved it into its own method

--- WorkingGameCrazyPutting/src/MainProject/PuttingSimulator.java
+++ WorkingGameCrazyPutting/src/MainProject/PuttingSimulator.java
@@ -2,6 +2,7 @@ package MainProject;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class PuttingSimulator {
 	private PuttingCourse course;
@@ -58,15 +59,7 @@ public class PuttingSimulator {
 		 * This is done to give a focus to direction when taking a shot that's too strong.
 		 */
 		
-		if(initial_ball_velocity.get_scalar()> maxV) {
-			double temp = initial_ball_velocity.get_x()/initial_ball_velocity.get_y();
-			double newX = Math.sqrt( (maxV*maxV)/(temp*temp +1));
-			double newY = newX/temp;
-			
-			initial_ball_velocity.set_x(newX);
-			initial_ball_velocity.set_y(newY);
-			
-		}
+		initial_Velocity_Check( initial_ball_velocity);
 		
 		this.velocity=initial_ball_velocity;
 		Vector2d temp= position;
@@ -97,6 +90,58 @@ public class PuttingSimulator {
 		if(course.is_put(position)) {
 			put();
 			System.out.println("You have putted, number of shots: "+shot);
+		}
+	}
+	
+	public ArrayList<Vector2d> take_shot_list(Vector2d initial_ball_velocity) {
+		ArrayList<Vector2d> ballPath= new ArrayList<Vector2d>();
+		System.out.println("This is shot #"+(++shot));
+		
+		/*
+		 * This is a system to make sure that a given shot is always going to be less than maxV
+		 * Not hard to implement, kind of weird.
+		 * Assumes that the new value should have the same relation between x and y (so if x is twice y then this new value will do that as well)
+		 * This is done to give a focus to direction when taking a shot that's too strong.
+		 */
+		
+		initial_Velocity_Check( initial_ball_velocity);
+		
+		this.velocity=initial_ball_velocity;
+		Vector2d temp= position;
+		boolean conti=true;
+		while(conti) {
+			acceleration=course.calculate_acceleration(position, velocity);
+			position=engine.solve(position, velocity);
+			ballPath.add(position);
+			if(course.is_water(position)) {
+				System.out.println("Your ball has gone into water, +1 shot penalty! \nCurrent Score: "+(++shot));
+				position=temp;
+				ballPath.add(position);
+				velocity=new Vector2d(0,0);
+				conti=false;
+				break;
+			}
+			
+			velocity=engine.solve(velocity, acceleration);
+			if(velocity.get_scalar()<stopV.get_scalar() && acceleration.get_scalar()< course.calculate_acceleration(position, stopV).get_scalar()) conti=false;
+		}
+		
+		if(course.is_put(position)) {
+			put();
+			System.out.println("You have putted, number of shots: "+shot);
+		}
+		return ballPath;
+	}
+	
+	private void initial_Velocity_Check(Vector2d initial_ball_velocity) {
+		if(initial_ball_velocity.get_scalar()> maxV) {
+			double temp = initial_ball_velocity.get_x()/initial_ball_velocity.get_y();
+			double newX = Math.sqrt( (maxV*maxV)/(temp*temp +1));
+			double newY = newX/temp;
+			
+			initial_ball_velocity.set_x(newX);
+			initial_ball_velocity.set_y(newY);
+			
 		}
 	}
 	
