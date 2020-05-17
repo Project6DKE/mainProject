0a6228e5a6fb941273ef694308404bc3b1cef508
Zigludo123 <56159408+Zigludo123@users.noreply.github.com>
2020-05-17 18:11:07 +0200

    New function that is actually really good for testing

--- WorkingGameCrazyPutting/src/MainProject/Tester.java
+++ WorkingGameCrazyPutting/src/MainProject/Tester.java
@@ -22,11 +22,12 @@ public class Tester {
 //		PuttingCourse course1 = xh.readFile();
 //		System.out.println(course1.get_height().toString());
 		
-		Function2d height= new FunctionH("2");
+		//Function2d height= new FunctionH("2");
 		//Function2d height = new FunctionH("-0.01 * x + 0.003 * x ^ 2 + 0.04 * y + 1");
+		Function2d height = new FunctionH("( 7 * x * y ) / ( exp ( x ^ 2 + y ^ 2 ) )");
 		
-		Vector2d flag = new Vector2d(10,10);
-		Vector2d start = new Vector2d(0,0);
+		Vector2d flag = new Vector2d(3,0);
+		Vector2d start = new Vector2d(0,2);
 		
 		double g,m,mu,vmax,tol;
 		g=9.81;m=45.93/1000;mu=0.131;vmax=10000;tol=0.03;
@@ -37,8 +38,8 @@ public class Tester {
 		
 		PuttingSimulator s= new PuttingSimulator(course,engine);
 		
-		BasicAI ai = new BasicAI(s);
-		//NewAI ai = new NewAI(s);
+		//BasicAI ai = new BasicAI(s);
+		NewAI ai = new NewAI(s);
 		//BackwardsAI ai = new BackwardsAI(s);
 		
 		while(!s.course_put) {
