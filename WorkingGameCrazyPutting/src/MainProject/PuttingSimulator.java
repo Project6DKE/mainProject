6990b47d53a7372d4eb94479cc05fc0936751ae0
HusamAQ <56165481+HusamAQ@users.noreply.github.com>
2020-05-17 14:57:18 +0200

    added restart_simulation() method

--- WorkingGameCrazyPutting/src/MainProject/PuttingSimulator.java
+++ WorkingGameCrazyPutting/src/MainProject/PuttingSimulator.java
@@ -323,4 +323,10 @@ public class PuttingSimulator {
 	public void setShot(int nbr) {
 		shot = nbr;
 	}
+	
+	public void restart_simulation() {
+		this.position=course.get_start_position();
+		this.velocity=new Vector2d(0,0);
+		this.shot=0;
+	}
 }
