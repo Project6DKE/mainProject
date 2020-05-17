7972437d3a5275a7c42ce20f76ea4fef24679096
Zigludo123 <56159408+Zigludo123@users.noreply.github.com>
2020-05-17 13:44:24 +0200

    Implementation of interfaces for BasicAI & NewAI

--- WorkingGameCrazyPutting/src/botFolder/BasicAI.java
+++ WorkingGameCrazyPutting/src/botFolder/BasicAI.java
@@ -1,9 +1,10 @@
 package botFolder;
 
+import MainProject.PuttingCourse;
 import MainProject.PuttingSimulator;
 import MainProject.Vector2d;
 
-public class BasicAI {
+public class BasicAI implements PuttingBot {
 
     /*
     Basics of the AI are in place, all that's needed now is for it to play the game until the shot has been put
@@ -16,6 +17,10 @@ public class BasicAI {
     
     static final double STOP0 = 0.00000000000001;
 
+    public BasicAI() {
+    	
+    }
+    
     public BasicAI(PuttingSimulator simulator){
         this.theGame = simulator;
         this.flag = simulator.getCourse().get_flag_position();
@@ -72,5 +77,40 @@ public class BasicAI {
     	}
     	
     }
+
+	@Override
+	public Vector2d shot_velocity(PuttingCourse course, Vector2d ball_position) {
+		
+		this.currentBallPosition = ball_position;
+		
+		double xdist = currentBallPosition.getXDistance(flag);
+        double ydist = currentBallPosition.getYDistance(flag);
+        
+        double vx0 = Math.signum(xdist)*STOP0;
+    	double vy0 = Math.signum(ydist)*STOP0;
+    	
+    	Vector2d speed = new Vector2d(vx0, vy0);
+        
+        
+        Vector2d acceleration = course.calculate_acceleration(currentBallPosition,speed);
+
+        //double maxV = theGame.course.get_maximum_velocity();
+        
+        double reqVelX = getVel(acceleration.get_x(),xdist);
+        double reqVelY = getVel(acceleration.get_y(),ydist);
+        
+        //double angle = currentBallPosition.get_angle(flag);
+
+        /*if(reqVelX>maxV){
+            reqVelY = maxV;
+        }*/
+        
+        /*
+        At this point I would return requiredVel somehow to take the shot, I'll update that after work
+         */
+
+        //theGame.take_angle_shot(requiredVel,angle);
+        return new Vector2d(reqVelX, reqVelY);
+	}
 
 }
