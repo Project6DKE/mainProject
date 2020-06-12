e2300d172076e3cfbd6bc6b358f7407190c187cd
Zigludo123 <56159408+Zigludo123@users.noreply.github.com>
2020-06-12 12:57:03 +0200

    Create BotTester.java
    Separate class implemented to make testing the bots much easier

---
+++ WorkingGameCrazyPutting/src/botFolder/BotTester.java
@@ -0,0 +1,42 @@
+package botFolder;
+
+import MainProject.PuttingCourse;
+import MainProject.PuttingSimulator;
+import MainProject.RungeKutta;
+import MainProject.Vector2d;
+import readingOfFunctions.Function2d;
+import readingOfFunctions.FunctionH;
+
+public class BotTester {
+	
+	public static void main(String[] args) throws Exception {
+		
+		// Three nice functions for testing
+		Function2d height= new FunctionH("2");
+		//Function2d height = new FunctionH("-0.01 * x + 0.003 * x ^ 2 + 0.04 * y");
+		//Function2d height = new FunctionH("( 7 * x * y ) / ( exp ( x ^ 2 + y ^ 2 ) )");
+		
+		Vector2d flag = new Vector2d(3,0);
+		Vector2d start = new Vector2d(0,2);
+		
+		double g,m,mu,vmax,tol;
+		g=9.81;m=45.93/1000;mu=0.131;vmax=10000;tol=0.03;
+		
+		PuttingCourse course = new PuttingCourse(height,flag, start, mu, vmax,tol,g,m );
+		
+		RungeKutta engine = new RungeKutta();
+		
+		PuttingSimulator s= new PuttingSimulator(course,engine);
+		
+		//BasicAI ai = new BasicAI(s);
+		//NewAI ai = new NewAI(s);
+		MergeAI ai = new MergeAI();
+		
+		Vector2d shot = ai.shot_velocity(course, s.get_ball_position());
+		
+		s.take_shot(shot);
+		
+		
+	}
+	
+}
