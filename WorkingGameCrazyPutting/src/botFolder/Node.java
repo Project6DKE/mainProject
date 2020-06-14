12506d3d96a628c5dac2e408e9dfed1125c5febc
KristianvK <vankuijk.kristian@gmail.com>
2020-06-14 12:24:50 +0200

    node update

--- WorkingGameCrazyPutting/src/botFolder/Node.java
+++ WorkingGameCrazyPutting/src/botFolder/Node.java
@@ -1,11 +1,46 @@
 package botFolder;
 
-public class Node {
-    public String value;
-    public Node left, right;
+import MainProject.Vector2d;
+import java.util.ArrayList;
 
-    public Node(String val){
-        this.value = val;
-        this.left = this.right = null;
+public class Node {
+	private Vector2d pos;
+	private ArrayList<Node> cameFrom;
+	private ArrayList<Node> neighbours;
+	
+	public Node(Vector2d pos, ArrayList<Node> cameFrom) {
+		this.pos = pos;
+		this.cameFrom = cameFrom;
+		this.neighbours = new ArrayList<>();
+		if(cameFrom == null) {
+			this.cameFrom = new ArrayList<>();
+		}
+	}
+	
+	public double distanceTo(Node nodeB) {
+		return this.pos.get_distance(nodeB.getPos());
+	}
+	
+	public void addNeighbour(Node node) {
+		this.neighbours.add(node);
+	}
+	
+	public void addToPath(Node node) {
+		this.cameFrom.add(node);
+		
+	}
+	
+	public ArrayList<Node> getNeighbours() {
+		return this.neighbours;
+	}
+	
+	public Vector2d getPos() {
+		return this.pos;
+	}
+	
+	public ArrayList<Node> getPath(){
+		return this.cameFrom;
     }
+	
+	
 }
