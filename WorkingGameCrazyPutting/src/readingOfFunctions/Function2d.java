78647c4c602d572687123f5ddfa8579d411a2a82
Zigludo123 <56159408+Zigludo123@users.noreply.github.com>
2020-06-20 17:49:59 +0200

    Modifications so that subfunctions can work

--- WorkingGameCrazyPutting/src/readingOfFunctions/Function2d.java
+++ WorkingGameCrazyPutting/src/readingOfFunctions/Function2d.java
@@ -5,4 +5,5 @@ import MainProject.Vector2d;
 public interface Function2d {
 		public double evaluate(Vector2d p);
 		public Vector2d gradient(Vector2d p);
+		public void add_subfunct(String string, double x_lower, double x_upper, double y_lower, double y_upper) throws Exception;
 }
